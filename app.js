08b19aa354a6409ac1a1f3e9c985e20e32340907
asmaasifchippa786 <asmaasifchippa786@gmail.com>
2022-05-26 13:46:48 +0500

    1st

---
+++ app.js
@@ -0,0 +1,23 @@
+var age = 12 ;
+
+alert("I am " + age + " years old");
+
+var num = 21;
+
+alert("You have visited this site " + num);
+
+var birthYear = 2009;
+
+document.write("<h1>Variables For Number</h1><hr/>");
+
+document.write("<h1>My birth year is " + birthYear + "</h1>");
+
+var visitorsName = "Sami Khan";
+
+var productTitle = "T-shirt(s)";
+
+var quantity = "3";
+
+var storeName = "XYZ Clothing store"
+
+document.write("<h1>" + visitorsName + " ordered " + productTitle + " " + quantity + " on " + storeName + "</h1>")
